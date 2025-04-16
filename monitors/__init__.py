546a979f7c8b7f7323f5db22d16adbf72ecea8a0
JeHeeYu <sees1234558@gmail.com>
2025-04-16 16:06:43 +0900

    feat: Add server montoring status

---
+++ monitors/__init__.py
@@ -0,0 +1,11 @@
+from .cpu_monitor import CpuMonitor
+from .ram_monitor import RamMonitor
+from .disk_monitor import DiskMonitor
+from .gpu_monitor import GpuMonitor
+
+__all__ = [
+    "CpuMonitor",
+    "RamMonitor",
+    "DiskMonitor",
+    "GpuMonitor"
+]
---
+++ monitors/ram_monitor.py
@@ -0,0 +1,13 @@
+import psutil
+
+class RamMonitor:
+    def get_usage(self):
+        mem = psutil.virtual_memory()
+        used_mb = (mem.total - mem.available) / 1024 / 1024
+        total_mb = mem.total / 1024 / 1024
+        percent = mem.percent
+        return {
+            'usage': f"{percent:.2f}",
+            'used': f"{used_mb:.0f}",
+            'total': f"{total_mb:.0f}"
+        }
